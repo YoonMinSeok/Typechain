e33bead02342de467c2d3ae477b2f5efd7d14d76
YoonMinSeok <81290520+YoonMinSeok@users.noreply.github.com>
2021-05-17 10:33:31 +0900

    Delete index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
-};
-console.log(sayHi("Nicolas", 24, "Male"));
-//# sourceMappingURL=index.js.map
